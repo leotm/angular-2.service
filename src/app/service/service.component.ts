b04fb62a928d32e1270616091100ae23598f8aed
LeoTM <leo.m@hotmail.co.uk>
2016-11-21 23:38:59 +0000

    Setup and fix project files

---
+++ src/app/service/service.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'si-service',
+  template: `
+    <si-cmp-a></si-cmp-a>
+    <si-cmp-b></si-cmp-b>
+  `,
+  styles: []
+})
+export class ServiceComponent {
+}

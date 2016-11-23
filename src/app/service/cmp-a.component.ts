ed6c6b3099e35a05a15ece232a157e2fc59c2403
LeoTM <leo.m@hotmail.co.uk>
2016-11-23 13:51:22 +0000

    Inject log service

--- src/app/service/cmp-a.component.ts
+++ src/app/service/cmp-a.component.ts
@@ -1,43 +1,48 @@
 import { Component } from '@angular/core';
 
+import { LogService } from './log.service';
+
 @Component({
-  selector: 'si-cmp-a',
-  template: `
-    <div>
-      <input type="text" #input>
-      <button (click)="onLog(input.value)">Log</button>
-      <button (click)="onStore(input.value)">Store</button>
-      <button (click)="onSend(input.value)">Send</button>
-    </div>
-    <hr>
-    <div>
-        <button (click)="onGet()">Refresh Storage</button>
-        <h3>Storage</h3>
-        <ul>
-            <li *ngFor="let item of items">{{item}}</li>
-        </ul>
-        <h3>Received Value</h3>
-        <p>{{value}}</p>
-    </div>
-  `
+    selector: 'si-cmp-a',
+    template: `
+		<div>
+			<input type="text" #input>
+			<button (click)="onLog(input.value)">Log</button>
+			<button (click)="onStore(input.value)">Store</button>
+			<button (click)="onSend(input.value)">Send</button>
+		</div>
+		<hr>
+		<div>
+				<button (click)="onGet()">Refresh Storage</button>
+				<h3>Storage</h3>
+				<ul>
+						<li *ngFor="let item of items">{{item}}</li>
+				</ul>
+				<h3>Received Value</h3>
+				<p>{{value}}</p>
+		</div>
+	`,
+    providers: [LogService]
 })
 export class CmpAComponent {
-  value = '';
-  items: string[] = [];
+    constructor(private logService: LogService) { }
 
-  onLog(value: string) {
+    value = '';
+    items: string[] = [];
 
-  }
+    onLog(value: string) {
+        this.logService.writeToLog(value);
+    }
 
-  onStore(value: string) {
+    onStore(value: string) {
 
-  }
+    }
 
-  onGet() {
+    onGet() {
 
-  }
+    }
 
-  onSend(value: string) {
+    onSend(value: string) {
 
-  }
+    }
 }
--- src/app/service/cmp-b.component.ts
+++ src/app/service/cmp-b.component.ts
@@ -1,39 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LogService } from './log.service';
+
 @Component({
-  selector: 'si-cmp-b',
-  template: `
-    <div>
-      <input type="text" #input>
-      <button (click)="onLog(input.value)">Log</button>
-      <button (click)="onStore(input.value)">Store</button>
-    </div>
-    <hr>
-    <div>
-        <button (click)="onGet()">Refresh Storage</button>
-        <h3>Storage</h3>
-        <ul>
-            <li *ngFor="let item of items">{{item}}</li>
-        </ul>
-        <h3>Received Value</h3>
-        <p>{{value}}</p>
-    </div>
-  `
+    selector: 'si-cmp-b',
+    template: `
+		<div>
+			<input type="text" #input>
+			<button (click)="onLog(input.value)">Log</button>
+			<button (click)="onStore(input.value)">Store</button>
+		</div>
+		<hr>
+		<div>
+				<button (click)="onGet()">Refresh Storage</button>
+				<h3>Storage</h3>
+				<ul>
+						<li *ngFor="let item of items">{{item}}</li>
+				</ul>
+				<h3>Received Value</h3>
+				<p>{{value}}</p>
+		</div>
+	`,
+    providers: [LogService]
 })
 export class CmpBComponent implements OnInit {
-  value = '';
-  items: string[] = [];
+    constructor(private logService: LogService) { }
+    value = '';
+    items: string[] = [];
 
-  onLog(value: string) {
-  }
+    onLog(value: string) {
+        this.logService.writeToLog(value);
+    }
 
-  onStore(value: string) {
-  }
+    onStore(value: string) {
+    }
 
-  onGet() {
-  }
+    onGet() {
+    }
 
-  ngOnInit() {
+    ngOnInit() {
 
-  }
+    }
 }

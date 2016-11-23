a805940eec30944f430619ede44e321017e18b11
LeoTM <leo.m@hotmail.co.uk>
2016-11-23 13:20:21 +0000

    Create log service

--- src/app/service/log.service.ts
+++ src/app/service/log.service.ts
@@ -1,8 +1,5 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
 export class LogService {
-
-  constructor() { }
-
+  writeToLog(logMessage: string) {
+    console.log(logMessage);
+  }
 }
